351e9e6d7c35b88500584fd84206e4bfabf710de
wil <wil@yopmail.com>
2024-11-02 14:21:58 +0200

    init

---
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor',
+        'pretty_version' => '3.25.x-dev',
+        'version' => '3.25.9999999.9999999-dev',
+        'reference' => 'c26d7db0c64c59b2e4d8b60318779cacaeda3f22',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor' => array(
+            'pretty_version' => '3.25.x-dev',
+            'version' => '3.25.9999999.9999999-dev',
+            'reference' => 'c26d7db0c64c59b2e4d8b60318779cacaeda3f22',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
